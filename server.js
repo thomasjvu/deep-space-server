1cc1902aa57a0bdfc437965bacd3afbf02fc2c96
Thomas J. Vu <thomasjvu@gmail.com>
2023-03-03 16:03:27 -0500

    Update server homepage

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 3005;
 
 const app = express();
+const cors = require("cors")
+
 
 // Connect to MongoDB
 connectDB();
 
 // Middleware
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
@@ -21,6 +24,12 @@ app.use(errorHandler);
 // Use Routes
 app.use("/users", require("./routes/userRoutes"));
 app.use("/notes", require("./routes/noteRoutes"));
+
+// Homepage for Server
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
 
 // Listen for open ports
 app.listen(PORT, () => {
